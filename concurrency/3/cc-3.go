00dc887a167d8022e48a112cdea254b67f1862b4
Mahmut Erkul <mahmut.erkul@kafein.com.tr>
2022-05-09 00:35:27 +0300

    ninja level 9 exercise #4

--- concurrency/3/cc-3.go
+++ concurrency/3/cc-3.go
@@ -12,18 +12,21 @@ func main() {
 	const delta = 100
 	wg.Add(delta)
 	runtime.GOMAXPROCS(8)
+	var mu sync.Mutex
 
 	var counter int = 0
 
 	for i := 0; i < delta; i++ {
 		go func() {
 			defer wg.Done()
+			mu.Lock()
 			temp := counter
 			runtime.Gosched()
 			temp++
 			counter = temp
-			fmt.Println("Number of Goroutines", runtime.NumGoroutine())
-			fmt.Println("Current value of counter", runtime.NumGoroutine())
+			mu.Unlock()
+			fmt.Printf("Number of Goroutines\t%d\n", runtime.NumGoroutine())
+			fmt.Printf("Current value of counter\t%d\n", runtime.NumGoroutine())
 		}()
 	}
 	wg.Wait()
