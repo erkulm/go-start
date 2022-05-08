68b051e17e99787ec64db770892b61726283a8c0
Mahmut Erkul <mahmut.erkul@kafein.com.tr>
2022-05-09 00:51:14 +0300

    ninja level 9 exercise #5

--- concurrency/3/cc-3.go
+++ concurrency/3/cc-3.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"runtime"
 	"sync"
+	"sync/atomic"
 )
 
 var wg sync.WaitGroup
@@ -12,23 +13,18 @@ func main() {
 	const delta = 100
 	wg.Add(delta)
 	runtime.GOMAXPROCS(8)
-	var mu sync.Mutex
 
-	var counter int = 0
+	var counter int32
 
 	for i := 0; i < delta; i++ {
 		go func() {
 			defer wg.Done()
-			mu.Lock()
-			defer mu.Unlock()
-			temp := counter
-			temp++
-			counter = temp
+			atomic.AddInt32(&counter, 1)
 			fmt.Printf("Number of Goroutines\t%d\n", runtime.NumGoroutine())
-			fmt.Printf("Current value of counter\t%d\n", counter)
+			fmt.Printf("Current value of counter\t%d\n", atomic.LoadInt32(&counter))
 		}()
 	}
 	wg.Wait()
-	fmt.Println(counter)
+	fmt.Println("end value", counter)
 
 }
